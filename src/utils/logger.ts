1e5f6167482acac0d2986b9adfcc97aff4e14fda
Abubakar-webmaker <m.abubakar.codes@gmail.com>
2026-08-02 20:05:29 +0500

    feat: add API service for folder management and logger utility

---
+++ src/utils/logger.ts
@@ -0,0 +1,8 @@
+const isDev = __DEV__;
+
+export const logger = {
+  info:  (...args: unknown[]): void => { if (isDev) console.log('[INFO]',  ...args); },
+  warn:  (...args: unknown[]): void => { if (isDev) console.warn('[WARN]',  ...args); },
+  error: (...args: unknown[]): void => { console.error('[ERROR]', ...args); },
+  debug: (...args: unknown[]): void => { if (isDev) console.log('[DEBUG]', ...args); },
+};
